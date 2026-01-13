80f6e849c6f324fe563220e113e4015918094609
Abraham Ayamigah <abkc070@gmail.com>
2026-01-13 23:14:22 +0000

    added networking configuration

---
+++ terraform/modules/networking/outputs.tf
@@ -0,0 +1,14 @@
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.web_sg.id
+}
+
+output "security_group_name" {
+  description = "Name of the security group"
+  value       = aws_security_group.web_sg.name
+}
+
+output "security_group_arn" {
+  description = "ARN of the security group"
+  value       = aws_security_group.web_sg.arn
+}
