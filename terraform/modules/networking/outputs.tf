83dd463c4358c0a092cdb69f03c540aefd83dd76
Abraham Ayamigah <abkc070@gmail.com>
2026-01-16 10:57:38 +0000

    Added data base for weeb app

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -12,3 +12,19 @@ output "security_group_arn" {
   description = "ARN of the security group"
   value       = aws_security_group.web_sg.arn
 }
+
+# Full-stack security groups
+output "frontend_security_group_id" {
+  description = "ID of the frontend security group"
+  value       = aws_security_group.frontend.id
+}
+
+output "backend_security_group_id" {
+  description = "ID of the backend security group"
+  value       = aws_security_group.backend.id
+}
+
+output "database_security_group_id" {
+  description = "ID of the database security group"
+  value       = aws_security_group.database.id
+}
